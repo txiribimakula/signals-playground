2815000fe6a632900a3df9b62a9a4c8c8da2472e
Pablo Tobalina <pablotobalina@gmail.com>
2023-12-06 11:15:32 +0100

    simulate consume cycle

--- src/app/consumer/consumer.component.ts
+++ src/app/consumer/consumer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SignalsService } from '../signals.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-consumer',
@@ -10,6 +11,28 @@ import { SignalsService } from '../signals.service';
 })
 export class ConsumerComponent {
   constructor(protected signals: SignalsService) {
-    signals.currentItem$.subscribe((item) => console.log(item + " in consumer component"));
+    this.consume();
+  }
+
+  consume() {
+    this.signals.currentItem$.subscribe((item) => {
+      console.log('consumed ' + item + ' in consumer component');
+      this.handle(item);
+    });
+  }
+
+  async handle(item: string) {
+    await this.work(item);
+    console.log('handled ' + item + ' in consumer component');
+    this.commit(item);
+  }
+
+  async work(item: string) {
+    await new Promise(_ => setTimeout(_, 3000));
+  }
+
+  commit(item: string) {
+    this.signals.offset.update((value) => value + 1);
+    console.log('commited ' + item + ' in consumer component');
   }
 }
